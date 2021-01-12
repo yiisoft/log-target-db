f60747915bccd70a52d072a8663e7fd398fcdd01
devanych <mail@devanych.ru>
2021-01-12 16:15:52 +0300

    Add configuration

---
+++ config/common.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Log\Logger;
+use Yiisoft\Log\Target\Db\DbTarget;
+
+return [
+    LoggerInterface::class => static fn (ContainerInterface $container) => new Logger([
+        new DbTarget($container->get(ConnectionInterface::class)),
+    ]),
+];
---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/yii-db-migration' => [
+        'createNamespace' => 'Yiisoft\Log\Target\Db\Migration',
+        'updateNamespace' => ['Yiisoft\Log\Target\Db\Migration'],
+    ],
+];
