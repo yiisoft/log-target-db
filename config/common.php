58d4eac258ac94c12faad37957231996705f8b55
devanych <mail@devanych.ru>
2021-01-24 19:39:23 +0300

    Remove redundant DbTarget declaration to config

--- config/common.php
+++ config/common.php
@@ -3,12 +3,9 @@
 declare(strict_types=1);
 
 use Psr\Log\LoggerInterface;
-use Yiisoft\Db\Connection\ConnectionInterface;
 use Yiisoft\Log\Logger;
 use Yiisoft\Log\Target\Db\DbTarget;
 
 return [
     LoggerInterface::class => static fn (DbTarget $dbTarget) => new Logger([$dbTarget]),
-
-    DbTarget::class => static fn (ConnectionInterface $db) => new DbTarget($db),
 ];
