a8af9b42ab4bc15bd158ddd9515f88893b854cec
devanych <mail@devanych.ru>
2021-01-25 16:32:27 +0300

    Remove createNamespace from config/params.php

--- config/params.php
+++ config/params.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     'yiisoft/yii-db-migration' => [
-        'createNamespace' => 'Yiisoft\Log\Target\Db\Migration',
         'updateNamespace' => ['Yiisoft\Log\Target\Db\Migration'],
     ],
 ];
