d5e8265a02ad16a9a844069d839a8b8dbb7d07dc
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-04-05 15:41:06 -0400

    Better naming classes with `PDO` - part 1. (#42)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,8 +11,8 @@ use Psr\Log\LoggerInterface;
 use Psr\SimpleCache\CacheInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Db\Sqlite\ConnectionPDO as SqlLiteConnection;
-use Yiisoft\Db\Sqlite\PDODriver;
+use Yiisoft\Db\Sqlite\Connection;
+use Yiisoft\Db\Sqlite\Driver;
 use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\ContainerConfig;
@@ -66,9 +66,9 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
                     LoggerInterface::class => Logger::class,
 
                     ConnectionInterface::class => [
-                        'class' => SqlLiteConnection::class,
+                        'class' => Connection::class,
                         '__construct()' => [
-                            new PDODriver('sqlite:' . __DIR__ . '/runtime/test.sq3'),
+                            new Driver('sqlite:' . __DIR__ . '/runtime/test.sq3'),
                         ],
                         'setLogger()' => [DynamicReference::to(Logger::class)],
                     ],
